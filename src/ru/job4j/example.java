fcd075aa06cce7699ec66522ca721f56ef28cced
Aleksandr <karkasson@list.ru>
2022-03-17 22:09:44 +0300

    update1

--- src/ru/job4j/example.java
+++ src/ru/job4j/example.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class example {
     public static void main(String[] args) {
 
-        System.out.println("1234567890");
+        System.out.println("12345678910");
 
     }
 
